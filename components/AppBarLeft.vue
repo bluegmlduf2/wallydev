5e59d273eb588c30a9cfcf91c6a56711775e990a
wally_yoon <bluegmlduf2@naver.com>
2022-09-17 22:09:31 +0900

    0917 app-bar컴포넌트 분리

---
+++ components/AppBarLeft.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-navigation-drawer v-model="drawer" clipped fixed app>
+    <v-treeview
+      v-model="tree"
+      :items="items"
+      activatable
+      item-key="name"
+      open-all
+      hoverable
+    >
+      <template slot="label" slot-scope="props">
+        <nuxt-link
+          v-if="props.item.to"
+          class="text-decoration-none black--text"
+          :to="props.item.to"
+        >
+          <v-icon>{{ props.item.icon }}</v-icon>
+          {{ props.item.name }}
+        </nuxt-link>
+        <span v-else class="black--text"
+          ><v-icon>{{ props.item.icon }}</v-icon
+          >{{ props.item.name }}</span
+        >
+      </template>
+    </v-treeview>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AppBarLeft',
+  data() {
+    return {
+      tree: [],
+      items: [
+        {
+          icon: 'mdi-home',
+          name: 'Home',
+          to: '/',
+        },
+        {
+          icon: 'mdi-human-greeting-variant',
+          name: 'Life',
+          children: [
+            {
+              name: '일본생활',
+              to: '/inspire',
+            },
+            {
+              name: '요리',
+              to: '/inspire',
+            },
+          ],
+        },
+        {
+          icon: 'mdi-laptop',
+          name: 'Development',
+          children: [
+            {
+              name: 'Javascript',
+              to: '/inspire',
+            },
+            {
+              name: 'VueJs',
+              to: '/inspire',
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,7 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" clipped fixed app>
-      <v-treeview
-        v-model="tree"
-        :items="items"
-        activatable
-        item-key="name"
-        open-all
-        @update:active="moveToPage"
-        
-      >
-        <template #prepend="{ item }">
-          <v-icon>{{ item.icon }}</v-icon>
-        </template>
-      </v-treeview>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="true" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon>
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <AppBarTop/>
+    <AppBarLeft/>
     <v-main>
       <v-container>
         <Nuxt />
@@ -32,55 +11,15 @@
 </template>
 
 <script>
+import AppBarLeft from '~/components/AppBarLeft.vue'
+import AppBarTop from '~/components/AppBarTop.vue'
 export default {
   name: 'DefaultLayout',
+  components: { AppBarLeft, AppBarTop },
   data() {
     return {
-      title: 'WALLY DEV',
-      drawer: false,
-      tree: [],
-      items: [
-        {
-          icon: 'mdi-home',
-          name: 'Home',
-          to: '/',
-        },
-        {
-          icon: 'mdi-human-greeting-variant',
-          name: 'Life',
-          to: '/inspire',
-          children: [
-            {
-              name: '일본생활',
-              to: '/inspire',
-            },
-            {
-              name: '요리',
-              to: '/inspire',
-            },
-          ],
-        },
-        {
-          icon: 'mdi-laptop',
-          name: 'Development',
-          children: [
-            {
-              name: 'Javascript',
-              to: '/inspire',
-            },
-            {
-              name: 'VueJs',
-              to: '/inspire',
-            },
-          ],
-        },
-      ],
+      
     }
-  },
-  methods: {
-    moveToPage(item) {
-      console.log(item)
-    },
   },
 }
 </script>
