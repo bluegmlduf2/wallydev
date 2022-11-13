56020611e7f1467e6124facfa8cece9bf4312e5b
wally_yoon <bluegmlduf2@naver.com>
2022-11-13 15:09:03 +0900

    1113 배포환경설정추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/wallydev/', // github repository 이름 넣기
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'WALLY DEV',
